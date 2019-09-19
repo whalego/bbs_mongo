a7fe2693345ae426c366fdc87d4cbd88f0d68486
admin <kujira5go5@gmail.com>
2019-09-19 18:01:39 +0900

    make login function

--- access/mongo.py
+++ access/mongo.py
@@ -3,11 +3,13 @@ from pymongo import MongoClient
 
 
 class DBConnection:
-    def __init__(self):
+    def __init__(self, db_name, collection_name):
         self.client = MongoClient("localhost")
-        self.db = self.client.test_database
-        self.collection = self.db.test_collection
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
 
+
+class DBPostMessage(DBConnection):
     def insert_db(self, user_name, text, pict_url=None):
         try:
             post = {
@@ -24,8 +26,39 @@ class DBConnection:
 
         return "succeed"
 
-    def insert_db_many(self):
-        pass
-
     def show_db_all(self):
         return [x for x in self.collection.find()]
+
+
+class DBAccount(DBConnection):
+    def create_account(self, account, password):
+        post = {"Account": account,
+                "Password": password
+                }
+        try:
+            if self.search_account(post["Account"]):
+                return "Already Exists"
+            else:
+                self.collection.insert_one(post)
+                return "succeed"
+        except Exception as e:
+            print(e)
+            return "failed"
+
+    def search_account(self, account):
+        try:
+            if account:
+                return self.collection.find_one({"Account": account})
+        except Exception as e:
+            print(e)
+
+        return None
+
+
+if __name__ == "__main__":
+    # db = DBPostMessage("test_database", "test_collection")
+    # db = DBAccount("UserDB", "UserCollections")
+    # a = db.search_account("_test_user")
+    # print(a)
+    pass
+
--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from werkzeug.utils import secure_filename
-from access.mongo import DBConnection
+from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin, current_user
+from access.mongo import DBPostMessage, DBAccount
 from pathlib import Path
 import datetime
-from PIL import Image
-import io
 app = Flask(__name__)
 app.secret_key = "asd"
-db = DBConnection()
+db = DBPostMessage("test_database", "test_collection")
+account_db = DBAccount("UserDB", "UserCollections")
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "users.login"
 
 
 @app.route("/")
-def index(message=""):
+def index():
     item_list = [{"author": x["author"],
                   "text": x["text"],
                   "date": x["date"],
                   "pict": x["pict"],
                   } for x in db.show_db_all()]
 
-    return render_template("/index.html", item_list=item_list, db_log=message)
+    return render_template("contents/index.html", item_list=item_list)
 
 
 @app.route("/form", methods=["POST"])
+@login_required
 def form():
     if request.method == "POST":
         req = request.form
@@ -36,6 +39,48 @@ def form():
         else:
             flash(result)
     return redirect(url_for("index"))
+
+
+@app.route("/login_form", methods=["POST"])
+def login_form():
+    if request.method == "POST":
+        user = User(request.form["account"])
+        if request.form["password"] == user.info["Password"]:
+            login_user(user)
+            flash(current_user.id)
+            return redirect(url_for("index"))
+
+    flash("login failed")
+    return redirect(url_for("index"))
+
+
+class User(UserMixin):
+    def __init__(self, name):
+        self.id = name
+        self.info = self._get_id()
+
+    def is_authenticated(self):
+        pass
+
+    def _get_id(self):
+        return account_db.search_account(self.id)
+
+
+@login_manager.user_loader
+def user_loader(account_name):
+    user = User(account_name)
+    if user.info and account_name == user.info["Account"]:
+        return user
+    return None
+
+
+@login_manager.request_loader
+def request_loader(req):
+    user = User(request.form["account"])
+    if user.info and user.info["Account"] != req.form["account"]:
+        return None
+    user.is_authenticated = request.form["account"] == user.info["Password"]
+    return user
 
 
 def save_picture(picture):
